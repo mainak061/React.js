cb110ef99f9e9f09f3f2ab06580fa1275f7cc5b9
mainak061 <mainak061@gmail.com>
2025-07-21 17:54:05 +0530

    .

--- 06router/src/components/Header/Header.jsx
+++ 06router/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            src="https://1000logos.net/wp-content/uploads/2021/10/Batman-Logo.png"
                             className="mr-3 h-12"
                             alt="Logo"
                         />
