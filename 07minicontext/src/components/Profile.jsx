67be47c23cc9782ec445e742baa85b091c27bf19
mainak061 <mainak061@gmail.com>
2025-07-25 13:28:38 +0530

    Basic knowladge about context

---
+++ 07minicontext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import UserContext from "../context/userContext"
+
+function Profile() {
+    const {user} = useContext(UserContext)
+
+    if (!user) return <div> <br />Please login</div>
+
+    return <div>welcome {user.username}</div>
+
+}
+
+export default Profile
